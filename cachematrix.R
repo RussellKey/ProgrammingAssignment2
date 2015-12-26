07aa3f01d3899eae46b1c594f0e8985a9c599604
keyruss <russell.key@gmail.com>
2015-12-25 18:39:34 -0800

    added code

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following two functions will create an R object to store a matrix and cache its inverse using lexical scoping
 
-## Write a short comment describing this function
+## Initial function called to store a matrix object and list of functions.
+## An emptry matrix is create if no parameters are provided.
+## Example : v <- makeCacheMatrix(matrix(c(1,2,3,4), nrow=2, ncol=2))
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	# Initialize variable to store inverse
+        i <- NULL
+	# Update internal variable with new matrix and reset inverse
+        set <- function(y) {
+                x <<- y
+                i <<- NULL
+        }
+	# Return matrix
+        get <- function() x
+	# Set and cache inverse
+        setSolve <- function(solve) i <<- solve
+	# Return inverse of matrix
+        getSolve <- function() i
+	# Create vector of available functions for this object
+        list(set = set, get = get, setSolve = setSolve, getSolve = getSolve)
 }
 
-
-## Write a short comment describing this function
+## Function to calculate inverse of matrix created with makeCacheMatrix.
+## Calculates inverse or returns cached inverse if it exists
+## Example : cacheSolve(v)
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+	# Get inverse 
+        i <- x$getSolve()
+	# Determine if inverse exists and return if it exists
+        if(!is.null(i)) {
+		# Return cached inverse
+                message("getting cached data")
+                return(i)
+        }
+	# Otherwise copy matrix into local variable
+        data <- x$get()
+	# Calaculate inverse
+        i <- solve(data, ...)
+	# Set (and cache) calculated inverse
+        x$setSolve(i)
+	# Return inverse
+        i
 }
